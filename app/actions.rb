ad8265fc26a0a742c84ca9d68e623dcb8b990d99
Kristin Lee <kristin.lee@lighthouselabs.com>
2023-11-14 04:59:21 +0000

    W3D1

--- app/actions.rb
+++ app/actions.rb
@@ -6,8 +6,8 @@ def humanized_time_ago(time_ago_in_minutes)
     end
   end
   
-  get '/' do
-    finstagram_post_spongebob = {
+  get '/' do 
+    @finstagram_post_spongebob = {
         username: "wholesome_spongebob",
         avatar_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUV6iIw6tNQARMJZ2rZ7uVHtJr0-wH1Nu0dQ&usqp=CAU",
         photo_url: "https://wallpapers.com/images/hd/cute-aesthetic-spongebob-cartoon-6m3a3fmyqfb2tsrq.jpg",
@@ -20,7 +20,7 @@ def humanized_time_ago(time_ago_in_minutes)
      }]
     }
 
-    finstagram_post_plankton = {
+    @finstagram_post_plankton = {
         username: "puny.plankton4",
         avatar_url: "https://i.pinimg.com/564x/e0/e2/8a/e0e28acf97e84b001e858e19c10bf3a9.jpg",
         photo_url: "https://i.pinimg.com/originals/9c/a3/80/9ca3805548f5e783733cfbc5a998ba32.jpg",
@@ -33,7 +33,7 @@ def humanized_time_ago(time_ago_in_minutes)
         }]
     }
 
-    finstagram_post_squidward = {
+    @finstagram_post_squidward = {
         username: "handsome-squidward",
         avatar_url: "https://i.pinimg.com/originals/e4/d9/50/e4d950f1332f136e7f9a21d6e499e949.jpg",
         photo_url: "https://media.tenor.com/lQ87s0dLeRYAAAAC/clarinet-squidward.gif",
@@ -42,11 +42,16 @@ def humanized_time_ago(time_ago_in_minutes)
         comment_count: 1,
         comments: [{
             username: "handsome-squidward",
-            text: "Thanks everyone for a great concert last night!"
+            text: "Thanks everyone for a great concert last night!", 
+        }, 
+        {   username: "wholesome_spongebob",
+            text: "I love you Squidward!"
         }]
     }
 
-    [finstagram_post_spongebob, finstagram_post_plankton, finstagram_post_squidward].to_s
+    @finstagram_posts = [@finstagram_post_spongebob, @finstagram_post_plankton, @finstagram_post_squidward]
+
+    erb(:index)
 end
 
 
